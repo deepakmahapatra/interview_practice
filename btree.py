9dcd9faba4d2890630e407a74e50fba932a679e8
deepakmahapatra <deepakmahapatra54@gmail.com>
2020-10-23 23:12:03 -0500

    completed bst code cases

--- btree.py
+++ btree.py
@@ -63,3 +63,92 @@ class Btree:
         temp = head.left
         head.left = head.right
         head.right = temp
+
+    def structurally_unique_count_trees(self, num_nodes):
+        """
+        finds the number of structurally unique btrees possible
+        :param: num_nodes
+        :return:
+        """
+        if num_nodes <= 1:
+            return 1
+        sum = 0
+        for i in range(num_nodes+1):
+            count_left_trees = self.structurally_unique_count_trees(i-1)
+            count_right_trees = self.structurally_unique_count_trees(num_nodes-1)
+
+            sum = sum + (count_left_trees * count_right_trees) # number of subtrees possible with i as the root
+        return sum
+
+    def print_nodes_in_range(self, node, high, low):
+        """
+        returns all nodes within a range in a binary search tree can be zero as well
+        check every node to see if value is in between the range
+        :param node:
+        :param high:
+        :param low:
+        :return:
+        """
+        if not node:
+            return
+        if low <= node.data:
+            self.print_nodes_in_range(node.left, low, high)
+        if low <= node.data <= high:
+            print(node.data)
+        if high > node.data:
+            self.print_nodes_in_range(node.right, low, high)
+
+
+    def is_bst(self, node, min=float('-inf'), max=float('inf')):
+        """
+        check if btree is a binary search tree
+
+        :param node:
+        :param min: Initially interger min
+        :param max: Initially integer max
+        :return:
+        """
+        if not node:
+            return True
+        if max < node.data <= min:
+            return False
+        return self.is_bst(node.left, min, node.data) and self.is_bst(node.right, node.data, max)
+
+    def sum_path_from_root(self, node, sum):
+        if not node.left and not node.right:
+            return sum == node.data
+        subsum = sum - node.data
+
+        if node.left:
+            has_path_sum = self.sum_path_from_root(node.left, subsum)
+            if has_path_sum:
+                return True
+        if node.right:
+            has_path_sum = self.sum_path_from_root(node.right, subsum)
+            if has_path_sum:
+                return True
+        return False
+
+    def print_all_path_root_to_leaf(self, node, path_list):
+        if not node:
+            return
+        path_list.append(node)
+        self.print_all_path_root_to_leaf(node.left, path_list)
+        self.print_all_path_root_to_leaf(node.right, path_list)
+        if not node.left and not node.right:
+            print(path_list)
+        path_list.remove(node)
+
+    def least_common_ancestor(self, node, node1, node2):
+        if not node:
+            return None
+        if node == node1 or node == node2:
+            return node
+        left_lca = self.least_common_ancestor(node.left, node1, node2)
+        right_lca = self.least_common_ancestor(node.right, node1, node2)
+
+        if left_lca and right_lca:
+            return node
+        if left_lca:
+            return left_lca
+        return right_lca
