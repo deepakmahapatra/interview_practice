defc81d886bda4715e9e75088ae4e16c54e91f95
deepakmahapatra <deepakmahapatra54@gmail.com>
2020-11-28 00:29:59 -0600

    dp without recursion

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -6,5 +6,16 @@ def count_no_of_ways_to_climb_stairs(n):
     return count_no_of_ways_to_climb_stairs(n-1) + count_no_of_ways_to_climb_stairs(n-2)
 
 
+def count_no_of_ways_to_climb(n):
+    s = []
+    s.append(0)
+    s.append(1)
+    s.append(2)
+    for i in range(3, n+1):
+        s.append(s[i-1]+s[i-2])
+    return s[n]
+
+
 if __name__ == '__main__':
     print(count_no_of_ways_to_climb_stairs(5))
+    print(count_no_of_ways_to_climb(5))
